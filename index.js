557aad3faf7b6ee8c86fb0e79bd51b0ccd677b82
Baskakovs <baskakovs@me.com>
2022-08-09 10:07:26 +0300

    commit tree dfd686803c1d86c791df4a400cc6a726a5dd5d3f

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 /* Your Code Here */
 
+const createEmployeeRecord = (array)=>{
+    const obj = {}
+    obj['firstName'] = array[0] 
+    obj['familyName'] = array[1] 
+    obj['title'] = array[2] 
+    obj['payPerHour'] = array[3] 
+    obj['timeInEvents'] = []
+    obj['timeOutEvents'] = []
+    return obj
+}
+
+function createEmployeeRecords(arrayOfArray){
+    return arrayOfArray.map((employee)=> createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(array, timeRecord){
+    let splitTimeRecord = timeRecord.split(" ")
+    let hours = parseFloat(splitTimeRecord[1])
+    array.timeInEvents = [{type: "TimeIn", date: splitTimeRecord[0], hour: hours}]
+    return array
+}
+
+function createTimeOutEvent(array, timeRecord){
+    let splitTimeRecord = timeRecord.split(" ")
+    let hours = parseFloat(splitTimeRecord[1])
+    array.timeOutEvents = [{type: "TimeOut", date: splitTimeRecord[0], hour: hours}]
+    return array 
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,4 +48,5 @@ const allWagesFor = function () {
 
     return payable
 }
+
 
